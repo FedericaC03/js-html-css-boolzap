0e20b39b31cdb16f15f5037694cb66084f3aa0e2
FedericaC03 <fdrk03@hotmail.it>
2020-09-08 19:17:07 +0200

    da sistemare js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
 
+  $("#send").keyup(
+    function(event) {
+      if (event.which == 13) {
+        var inputValue = $("#send").val();
+
+        if (inputValue != "") {
+          var newMessage = $(".clone").clone();
+          newMessage.prepend(inputValue);
+          $("main").append(newMessage);
+          $("#send").val("");
+          $(".clone").removeClass(".none");
+        }
+      }
+    }
+  )
+});
