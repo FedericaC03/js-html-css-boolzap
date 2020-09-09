6066efe945714db1ff73e9215286fa1ad122989d
FedericaC03 <fdrk03@hotmail.it>
2020-09-09 14:31:39 +0200

    FIXED TEMPLATE

--- js/script.js
+++ js/script.js
@@ -1,18 +1,52 @@
 $(document).ready(function() {
-
+  //SEND MESSAGE WITH ENTER
   $("#send").keyup(
     function(event) {
       if (event.which == 13) {
-        var inputValue = $("#send").val();
-
-        if (inputValue != "") {
-          var newMessage = $(".clone").clone();
-          newMessage.prepend(inputValue);
-          $("main").append(newMessage);
-          $("#send").val("");
-          $(".clone").removeClass(".none");
-        }
+        sendMessage();
       }
     }
-  )
+  );
+
+//UPON CLICKING ON INPUT, THE MICROPHONE ICON CHANGES TO AIRPLANE
+  $("#send").click(
+    function() {
+      $("#microphone").hide();
+      $("#paper-plane").show();
+    }
+  );
+
+//SEND MESSAGE BY CLICKING ON PAPERPLANE
+  $("#paper-plane").click(
+    function() {
+      sendMessage();
+    }
+  );
+
+//AFTER MY MESSAGE, THE CPU WILL ANSWER WITH OK AFTER 1 SECOND
+
 });
+
+//FUNCTION SEND MESSAGE
+function sendMessage() {
+var inputValue = $("#send").val();
+
+if (inputValue != "") {
+ var newMessage = $(".template .clone").clone();
+
+ var date = new Date();
+ var hours = date.getHours();
+ var minutes = date.getMinutes();
+
+ if (minutes < 10) {
+   var time = hours + ":0" + minutes;
+ } else {
+   var time = hours + ":" + minutes;
+ }
+
+  newMessage.children(".b-green").prepend(inputValue);
+  newMessage.find(".b-green .time").text(time);
+  $(".chat").append(newMessage);
+  $("#send").val("");
+ }
+}
